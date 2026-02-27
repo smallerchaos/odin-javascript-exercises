13a28688f608dc1e1353fddd7c145885947e6d37
smallerchaos <257726345+smallerchaos@users.noreply.github.com>
2026-02-27 20:34:51 +0100

    Add solution and reflections to 15 getTheTitles

--- foundations/15_getTheTitles/getTheTitles.js
+++ foundations/15_getTheTitles/getTheTitles.js
@@ -1,6 +1,12 @@
-const getTheTitles = function() {
-
+const getTheTitles = function(array) {
+    return array.map((book) => book.title,[]);
 };
+
+/*
+Thoughts and Reflections
+
+ABSOLUTELY NAILED IT. My solution was the exact same as the provided solution! I tried it with reduce at first but it didn't seem to be working but then it occured to me that I'm just kind of... transcribing, is the best way I can put it, the array so map should work just fine. 
+*/
 
 // Do not edit below this line
 module.exports = getTheTitles;
